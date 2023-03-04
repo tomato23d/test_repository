ebbc7b2f2ef09fe0ada59f52b30e632eecd3f5ee
tomato23d <anna.a.art@gmail.com>
2023-03-04 13:20:26 +1100

    exercise is completed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,12 @@
-window.alert("this should work")
+
+
+var answer = window.prompt("give me a number","");
+
+
+
+
+localStorage.setItem('myCat', answer);
+
+var answer2 = localStorage.getItem('myCat');
+
+console.log(answer2)
